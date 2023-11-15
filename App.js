691c6d2a92feab323da688159441217c1c26f2b7
harjasilin <harjasilin.01@gmail.com>
2023-11-15 15:13:04 +0530

    Commit

--- App.js
+++ App.js
@@ -29,24 +29,6 @@ const App = () => {
   );
 }
 
-const styles = StyleSheet.create({
-  centerText: {
-    flex: 1,
-    fontSize: 18,
-    padding: 32,
-    color: '#777'
-  },
-  textBold: {
-    fontWeight: '500',
-    color: '#000'
-  },
-  buttonText: {
-    fontSize: 21,
-    color: 'rgb(0,122,255)'
-  },
-  buttonTouchable: {
-    padding: 16
-  }
-});
+
 
 export default App;
